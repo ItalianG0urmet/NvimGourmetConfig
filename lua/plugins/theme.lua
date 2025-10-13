ede6fceed631c56b93b363448ac727927d8b449b
ItalianG0urmet <insogna_@outlook.it>
2025-10-13 22:50:54 +0200

    new theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,9 +1,29 @@
 -- plugins/theme.lua
 return {
-  "zootedb0t/citruszest.nvim",
-  priority = 1000,
-  lazy = false,
-  config = function()
-    vim.cmd("colorscheme citruszest")
-  end,
+    "bluz71/vim-moonfly-colors",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        vim.cmd("colorscheme moonfly")
+
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        vim.api.nvim_set_hl(0, "FloatBorder", { fg = "none", bg = "none" })
+
+        local border_groups = {
+            "LspInfoBorder",
+            "CmpBorder",
+            "TelescopeBorder",
+            "TelescopePromptBorder",
+            "TelescopeResultsBorder",
+            "TelescopePreviewBorder",
+            "NoiceCmdlinePopupBorder",
+            "NoicePopupBorder",
+            "WhichKeyBorder",
+            "NotifyBorder",
+        }
+
+        for _, group in ipairs(border_groups) do
+            vim.api.nvim_set_hl(0, group, { fg = "none", bg = "none" })
+        end
+    end,
 }
