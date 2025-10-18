c74d09453c41f4779ed46be8f967682fab30e132
ItalianG0urmet <insogna_@outlook.it>
2025-10-18 20:28:52 +0200

    Option update

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,43 +1,95 @@
--- core/options.lua: Core settings and UI
+-- core/options.lua
 local o = vim.opt
 
--- file
+-- File Handling 
 o.backup      = false
 o.writebackup = false
 o.swapfile    = false
+o.undofile    = true
+o.backupdir   = vim.fn.stdpath("data") .. "/backup//"
 
--- theme
-o.winborder = 'rounded'
-o.fillchars = { eob = ' ' }
-
--- editor UI
-o.number       = true
+-- Appearance / UI 
+o.termguicolors = true
+o.winborder     = "rounded"
+o.number        = true
 o.relativenumber = true
-o.showcmd      = true
-o.showmatch    = true
-o.wildmenu     = true
-o.title        = true
-o.ruler        = true
+o.signcolumn    = "yes:1"
+o.ruler         = true
+o.title         = true
+o.showcmd       = true
+o.showmatch     = true
+o.wildmenu      = true
+o.list          = true
+o.listchars     = { tab = "▸ ", extends = "❯", precedes = "❮", nbsp = "␣" }
+o.linebreak     = true
+o.showbreak     = "↪"
+o.fillchars     = {
+  fold = " ",
+  foldsep = " ",
+  foldopen = "",
+  foldclose = "",
+  vert = "│",
+  eob = " ",
+  msgsep = "‾",
+  diff = "╱",
+}
+o.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor20"
 
--- indentation
-o.tabstop      = 4
-o.shiftwidth   = 4
-o.expandtab    = true
-o.smartindent  = true
-o.autoindent   = true
+-- Splits 
+o.splitbelow = true
+o.splitright = true
+o.splitkeep  = "screen"
 
--- search
-o.ignorecase   = true
-o.smartcase    = true
-o.incsearch    = true
-o.hlsearch     = true
+-- Indentation 
+o.tabstop     = 4
+o.shiftwidth  = 4
+o.softtabstop = 4
+o.expandtab   = true
+o.smartindent = true
+o.autoindent  = true
 
--- other
-o.scrolloff    = 5
-o.mouse        = "a"
-o.clipboard    = "unnamedplus"
-o.backspace    = { "indent", "eol", "start" }
-o.timeoutlen = 300
+-- Search 
+o.ignorecase  = true
+o.smartcase   = true
+o.incsearch   = true
+o.hlsearch    = true
+
+-- Performance / Timing 
+o.timeoutlen  = 500
 o.ttimeoutlen = 10
-o.updatetime = 300
+o.updatetime  = 500
+o.scrolloff   = 5
+
+-- Editing Behavior
+o.mouse      = "a"
+o.clipboard  = "unnamedplus"
+o.backspace  = { "indent", "eol", "start" }
+
+-- File Navigation 
+o.wildignore = {
+  "*.o","*.obj","*.dll","*.exe","*.jpg","*.png","*.jpeg","*.gif","*.svg",
+  "*.pyc","*.pkl","*.DS_Store","*/__pycache__/*","*/.git/*","*/build/*",
+}
+o.wildignorecase = true
+
+-- Grep (ripgrep) 
+if vim.fn.executable("rg") == 1 then
+  o.grepprg = "rg --vimgrep --no-heading --smart-case"
+  o.grepformat = "%f:%l:%c:%m"
+end
+
+-- Diff
+o.diffopt = {
+  "vertical",
+  "filler",
+  "closeoff",
+  "context:3",
+  "internal",
+  "indent-heuristic",
+  "algorithm:histogram",
+  vim.fn.has("nvim-0.12") == 1 and "inline:char" or "linematch:60",
+}
+
+-- Misc
 vim.lsp.set_log_level("off")
+
