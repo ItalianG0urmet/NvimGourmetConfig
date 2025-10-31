f7628b053385c76b7b81290dc026855a9df64126
gourmet <insogna_@outlook.it>
2025-10-31 22:54:41 +0100

    fix background

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -7,7 +7,6 @@ return {
             stages = "fade_in_slide_out",
             timeout = 3000,
             render = "compact",
-            background_colour = "#1e1e2e",
             max_width = function()
                 return math.floor(vim.o.columns * 0.3)
             end,
