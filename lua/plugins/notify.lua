b0136f5530efec70b5b2c052165489a85e1437f8
gourmet <insogna_@outlook.it>
2025-11-07 10:32:34 +0100

    Update design

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -4,16 +4,10 @@ return {
     event = "VimEnter",
     config = function()
         require("notify").setup({
-            stages = "fade_in_slide_out",
-            timeout = 3000,
-            render = "compact",
+            --stages = "fade_in_slide_out",
+            timeout = 5000,
+            render = "wrapped-compact",
             background_colour = "#000000",
-            max_width = function()
-                return math.floor(vim.o.columns * 0.3)
-            end,
-            max_height = function()
-                return math.floor(vim.o.lines * 0.3)
-            end,
         })
         vim.notify = require("notify")
     end,
